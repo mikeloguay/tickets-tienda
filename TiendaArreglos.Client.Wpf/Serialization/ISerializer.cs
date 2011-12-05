5cc2373569e61ac20527f6b410be6e2827f2dd72
Miguel Fernández <10994206+mikeloguay@users.noreply.github.com>
2011-12-05 23:48:50 +0000

    ¡Primer commit!

---
+++ TiendaArreglos.Client.Wpf/Serialization/ISerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace TiendaArreglos.Client.Wpf.Serialization
+{
+    public interface ISerializer<T>
+    {
+        T DeserializeObject(string fileName);
+
+        void SerializeObject(T objectToSerialize, string fileName);
+    }
+}
---
+++ TiendaArreglos.Client.Wpf/Serialization/SerializerBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace TiendaArreglos.Client.Wpf.Serialization
+{
+    public class SerializerBase<T> : ISerializer<T>
+    {
+        public T DeserializeObject(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            XmlSerializer x = new XmlSerializer(typeof(T));
+            T objectToDeserialize = (T)x.Deserialize(fs);
+            fs.Close();
+            return objectToDeserialize;
+        }
+
+        public void SerializeObject(T objectToSerialize, string fileName)
+        {
+            // Create the XmlSerializer.
+            XmlSerializer s = new XmlSerializer(typeof(T));
+
+            // To write the file, a TextWriter is required.
+            TextWriter writer = new StreamWriter(fileName);
+
+            // Serialize the object, and close the TextWriter.      
+            s.Serialize(writer, objectToSerialize);
+            writer.Close();
+        }
+    }
+}
