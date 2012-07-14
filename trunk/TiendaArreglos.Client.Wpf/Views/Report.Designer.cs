2a1579790fb2f2b4bd50c33e9191cc98db57c417
mikeloguay <mikeloguay@a013494f-cf53-48a4-825c-6199ff317a4f>
2012-07-14 19:50:05 +0000

    - Report updated: logo in all pages

--- trunk/TiendaArreglos.Client.Wpf/Views/Report.Designer.cs
+++ trunk/TiendaArreglos.Client.Wpf/Views/Report.Designer.cs
@@ -30,14 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.TicketBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.ticketReportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.TicketBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.TicketBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // TicketBindingSource
-            // 
-            this.TicketBindingSource.DataSource = typeof(TiendaArreglos.Client.Wpf.Model.Ticket);
             // 
             // ticketReportViewer
             // 
@@ -48,14 +44,18 @@
             this.ticketReportViewer.LocalReport.ReportEmbeddedResource = "TiendaArreglos.Client.Wpf.Reports.Tickets.rdlc";
             this.ticketReportViewer.Location = new System.Drawing.Point(0, 0);
             this.ticketReportViewer.Name = "ticketReportViewer";
-            this.ticketReportViewer.Size = new System.Drawing.Size(284, 262);
+            this.ticketReportViewer.Size = new System.Drawing.Size(674, 570);
             this.ticketReportViewer.TabIndex = 0;
+            // 
+            // TicketBindingSource
+            // 
+            this.TicketBindingSource.DataSource = typeof(TiendaArreglos.Client.Wpf.Model.Ticket);
             // 
             // Report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(674, 570);
             this.Controls.Add(this.ticketReportViewer);
             this.Name = "Report";
             this.Text = "Report";
